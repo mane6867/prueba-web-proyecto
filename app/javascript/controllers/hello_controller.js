00edfe3cebc5d55a1a6380500961e164fac7ec8f
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-04-01 18:23:28 -0300

    18 23 commit

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Ha sorros isa y andrew!"
+    this.element.textContent = "Hello world!"
   }
 }
