b0e86d1783210517b3bf46f7fb1278ff10cb6eb3
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-04-01 18:02:16 -0300

    segundo commit prueba1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'render/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root 'render#index'
+  
+
+  root :to => 'render#index'
 end
